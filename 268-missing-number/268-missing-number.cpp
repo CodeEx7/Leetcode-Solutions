9933d5d05876a87718697f4683602173be2199f5
Brij Bhushan Maurya <62421744+Strange1-7@users.noreply.github.com>
2022-05-28 12:43:27 +0530

    Time: 46 ms (11.07%), Space: 24.1 MB (5.21%) - LeetHub

---
+++ 268-missing-number/268-missing-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n=nums.size();
+        unordered_set<int> st;
+        for(auto e:nums){
+            st.insert(e);
+        }
+        for(int i=0;i<n;i++){
+            if(st.find(i)==st.end())
+                return i;
+        }
+        return n;
+    }
+};
