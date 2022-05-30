d4e70e7e7721147d1c41190b58494e7af162d05a
Brij Bhushan Maurya <62421744+Strange1-7@users.noreply.github.com>
2022-05-30 23:48:19 +0530

    Time: 22 ms (57.82%), Space: 8.5 MB (48.50%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
-    int lengthOfLongestSubstring(string s) {
-        int left = 0, len = 0;
+     int lengthOfLongestSubstring(string s) {
+        int i = 0, len = 0;
         map<char, int> dict;
-        for (int right=0; right<s.size(); right++) {
-            if (dict.find(s[right]) != dict.end()) {
-                left = max(left, dict[s[right]] + 1);
+        for (int j=0; j<s.size(); j++) {
+            if (dict.find(s[j]) != dict.end()) {
+                i = max(i, dict[s[j]] + 1);
             }
-            dict[s[right]] = right;
-            len = max(len, right - left + 1);
+            dict[s[j]] = j;
+            len = max(len, j - i + 1);
         }
         return len;
     }
