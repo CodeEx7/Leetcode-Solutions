1794329c5638e5cf04405db43de2cf265a530b38
Brij Bhushan Maurya <62421744+Strange1-7@users.noreply.github.com>
2022-06-12 23:10:13 +0530

    Time: 79 ms (29.19%), Space: 41.4 MB (81.40%) - LeetHub

--- 1315-sum-of-nodes-with-even-valued-grandparent/1315-sum-of-nodes-with-even-valued-grandparent.cpp
+++ 1315-sum-of-nodes-with-even-valued-grandparent/1315-sum-of-nodes-with-even-valued-grandparent.cpp
@@ -12,16 +12,16 @@
 class Solution {
 public:
     
-    int helper(TreeNode* root){
-     if(root==NULL) return 0;
-        int summ=0;
-        if(root->val%2==0){
-            if(root->left != NULL && root->left->left != NULL) summ += root->left->left->val;
-            if(root->left != NULL && root->left->right != NULL) summ += root->left->right->val;
-            if(root->right != NULL && root->right->left != NULL) summ += root->right->left->val;
-            if(root->right != NULL && root->right->right != NULL) summ += root->right->right->val;
-        }
-        return summ + helper(root->left) + helper(root->right);
+int helper(TreeNode* root){
+    if(root==NULL) return 0;
+    int summ=0;
+    if(root->val%2==0){
+        if(root->left != NULL && root->left->left != NULL) summ += root->left->left->val;
+        if(root->left != NULL && root->left->right != NULL) summ += root->left->right->val;
+        if(root->right != NULL && root->right->left != NULL) summ += root->right->left->val;
+        if(root->right != NULL && root->right->right != NULL) summ += root->right->right->val;
+    }
+    return summ + helper(root->left) + helper(root->right);
  }
     //main working function
     int sumEvenGrandparent(TreeNode* root) {
