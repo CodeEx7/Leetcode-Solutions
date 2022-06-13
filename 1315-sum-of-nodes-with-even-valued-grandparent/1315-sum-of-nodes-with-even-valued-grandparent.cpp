eab6876dff00613ae6b06e8b86cc9366e3c9d83c
Brij Bhushan Maurya <62421744+Strange1-7@users.noreply.github.com>
2022-06-14 03:23:47 +0530

    Time: 53 ms (80.67%), Space: 41.4 MB (42.57%) - LeetHub

--- 1315-sum-of-nodes-with-even-valued-grandparent/1315-sum-of-nodes-with-even-valued-grandparent.cpp
+++ 1315-sum-of-nodes-with-even-valued-grandparent/1315-sum-of-nodes-with-even-valued-grandparent.cpp
@@ -1,27 +1,30 @@
 /**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
+ *Definition for a binary tree node.
+ *struct TreeNode {
+ *    int val;
+ *    TreeNode * left;
+ *    TreeNode * right;
+ *    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *};
  */
-class Solution {
-public:
-    
-    //main working function
-    int sumEvenGrandparent(TreeNode* root) {
-        int summ=0;
-        if(root==NULL) return summ;
-        if(root->val%2==0){
-        if(root->left != NULL && root->left->left != NULL) summ += root->left->left->val;
-        if(root->left != NULL && root->left->right != NULL) summ += root->left->right->val;
-        if(root->right != NULL && root->right->left != NULL) summ += root->right->left->val;
-        if(root->right != NULL && root->right->right != NULL) summ += root->right->right->val;
-    }
-    return summ + sumEvenGrandparent(root->left) + sumEvenGrandparent(root->right);
-    }
+class Solution
+{
+    public:
+
+       	//main working function
+        int sumEvenGrandparent(TreeNode *root)
+        {
+            int summ = 0;
+            if (root == NULL) return summ;
+            if (root->val % 2 == 0)
+            {
+                if (root->left != NULL && root->left->left != NULL) summ += root->left->left->val;
+                if (root->left != NULL && root->left->right != NULL) summ += root->left->right->val;
+                if (root->right != NULL && root->right->left != NULL) summ += root->right->left->val;
+                if (root->right != NULL && root->right->right != NULL) summ += root->right->right->val;
+            }
+            return summ + sumEvenGrandparent(root->left) + sumEvenGrandparent(root->right);
+        }
 };
