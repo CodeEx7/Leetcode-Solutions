ae8b66e91c4fcfa10dea17f60ae7d97187d82407
Brij Bhushan Maurya <62421744+Strange1-7@users.noreply.github.com>
2022-06-19 00:26:05 +0530

    Time: 1750 ms (34.25%), Space: 557.9 MB (84.91%) - LeetHub

---
+++ 211-design-add-and-search-words-data-structure/211-design-add-and-search-words-data-structure.cpp
@@ -0,0 +1,47 @@
+class WordDictionary {
+public:
+    struct node{
+        array<node*,26> child;
+        bool isend=0;
+        node(){
+            for(int i=0;i<26;++i){
+                child[i]=NULL;
+            }
+            isend=0;
+        }
+    };
+    node *root=NULL;
+    WordDictionary() {
+        root=new node();
+    }
+    void addWord(string word) {
+        node *t=root;
+        for(char ch:word){
+            if(t->child[ch-'a']==NULL)
+                t->child[ch-'a']=new node();
+            t=t->child[ch-'a'];
+        }
+        t->isend=1;
+    }
+    bool find(const string &word,int index,node* t_root){
+        int size=word.size();
+        for(;index<size;++index){
+            if(word[index]=='.'){
+                for(int i=0;i<26;++i){
+                    if(t_root->child[i] and find(word,index+1,t_root->child[i])){
+                        return 1;
+                    }
+                }
+                return 0;
+            }else if(t_root->child[word[index]-'a']){
+                t_root=t_root->child[word[index]-'a'];
+            }else{
+                return 0;
+            }
+        }
+        return t_root->isend;
+    }
+    bool search(string word){
+        return find(word,0,root);
+    }
+};
