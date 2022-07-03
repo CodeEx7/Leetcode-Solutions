0592633eb7807e7905ba82eab17d9b5d4ce85ecc
Brij Bhushan Maurya <62421744+Strange1-7@users.noreply.github.com>
2022-07-04 01:35:36 +0530

    Time: 0 ms (100.00%), Space: 7 MB (75.53%) - LeetHub

--- 376-wiggle-subsequence/376-wiggle-subsequence.cpp
+++ 376-wiggle-subsequence/376-wiggle-subsequence.cpp
@@ -1,21 +1,24 @@
 class Solution {
 public:
-    int wiggleMaxLength(vector<int>& nums) {
+    int wiggleMaxLength(vector<int>& nums) { //(3,3,3,2,5)
         if(nums.size()<=1) return 1;
         int res;
-        int prev=nums[1]-nums[0];
+        int prev=nums[1]-nums[0]; //prev=0
         if(prev==0){
-            res=1;
+            res=1; //sjdgwelkfjhk
         }else{
             res=2;
         }
-        for(int i=2;i<nums.size();i++){
-            int diff=nums[i]-nums[i-1];
-            if((diff>0 && prev<=0) || (diff<0 && prev>=0)){
-                res++;
-                prev=diff;
+        for(int i=2;i<nums.size();i++){ //nums[5]=5
+            int diff=nums[i]-nums[i-1]; //diff=3
+            if((diff>0 && prev<=0) || (diff<0 && prev>=0)){ 
+                res++; //res=3
+                prev=diff; //prev=3
             }
         }
         return res;
     }
 };
+
+//O(n)
+//O(1)
