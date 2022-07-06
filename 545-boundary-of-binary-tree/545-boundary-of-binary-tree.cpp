cb666ec6f3ec79bef85dacf6f8873ef8cf41c526
Brij Bhushan Maurya <62421744+Strange1-7@users.noreply.github.com>
2022-07-07 03:10:55 +0530

    Time: 19 ms (35.40%), Space: 20.9 MB (82.60%) - LeetHub

--- 545-boundary-of-binary-tree/545-boundary-of-binary-tree.cpp
+++ 545-boundary-of-binary-tree/545-boundary-of-binary-tree.cpp
@@ -9,66 +9,34 @@
  *    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  *};
  */
-class Solution
-{
-    public:
-        void leftside(vector<int> &ans, TreeNode *root)
-        {
-            if (root == NULL)
-                return;
-            if (root->left)
-            {
-                ans.push_back(root->val);
-                leftside(ans, root->left);
-            }
-            else if (root->right)
-            {
-                ans.push_back(root->val);
-                leftside(ans, root->right);
-            }
-        }
-    void rightside(vector<int> &ans, TreeNode *root)
-    {
-        if (root == NULL)
-            return;
-        if (root->right)
-        {
-            rightside(ans, root->right);
-            ans.push_back(root->val);
-        }
-        else if (root->left)
-        {
-            rightside(ans, root->left);
-            ans.push_back(root->val);
-        }
+class Solution {
+public:
+	vector<int> ans;
+    bool isLeaf(TreeNode* root) {
+        return root->left == nullptr && root->right == nullptr;
     }
-    void leafTreeNode(vector<int> &ans, TreeNode *root)
-    {
-        if (root == NULL)
-            return;
-        else if (root->left == NULL && root->right == NULL)
-        {
-            ans.push_back(root->val);
-            return;
-        }
-        else
-        {
-            leafTreeNode(ans, root->left);
-            leafTreeNode(ans, root->right);
-        }
-    }
-
-    vector<int> boundaryOfBinaryTree(TreeNode *root)
-    {
-        vector<int> ans;
-        if (root == NULL)
-            return ans;
+    void leftBoundary(TreeNode* root){
+        if (!root || isLeaf(root)) return;
         ans.push_back(root->val);
-        if (root->left == NULL && root->right == NULL)
-            return ans;
-        leftside(ans, root->left);
-        leafTreeNode(ans, root);
-        rightside(ans, root->right);
+        leftBoundary(root->left ? root->left: root->right);
+    }
+    void leaves(TreeNode* root){
+        if(!root) return;
+        leaves(root->left);
+        if(isLeaf(root)) ans.push_back(root->val);
+        leaves(root->right);
+    }
+    void rightBoundary(TreeNode* root){
+        if (!root || isLeaf(root)) return;
+        rightBoundary(root->right ? root->right: root->left);
+        ans.push_back(root->val);
+    }
+    vector<int> boundaryOfBinaryTree(TreeNode* root) {
+        if(!root) return ans;
+        ans.push_back(root->val);
+        leftBoundary(root->left);
+        if(!isLeaf(root)) leaves(root);
+        rightBoundary(root->right);
         return ans;
     }
 };
