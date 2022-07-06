431a4b1993f58eb7f383df7d1dab1e846b21ec16
Brij Bhushan Maurya <62421744+Strange1-7@users.noreply.github.com>
2022-07-07 03:17:52 +0530

    Time: 21 ms (23.97%), Space: 21 MB (58.03%) - LeetHub

--- 545-boundary-of-binary-tree/545-boundary-of-binary-tree.cpp
+++ 545-boundary-of-binary-tree/545-boundary-of-binary-tree.cpp
@@ -11,30 +11,29 @@
  */
 class Solution {
 public:
-	
-    void leftBoundary(TreeNode* root, vector<int> &ans){
+	vector<int> ans;
+    void leftBoundary(TreeNode* root){
         if (!root || (root->left == NULL && root->right == NULL)) return;
         ans.push_back(root->val);
-        leftBoundary((root->left ? root->left: root->right), ans);
+        leftBoundary(root->left ? root->left: root->right);
     }
-    void leaves(TreeNode* root, vector<int> &ans){
+    void leaves(TreeNode* root){
         if(!root) return;
-        leaves(root->left, ans);
+        leaves(root->left);
         if((root->left == NULL && root->right == NULL)) ans.push_back(root->val);
-        leaves(root->right, ans);
+        leaves(root->right);
     }
-    void rightBoundary(TreeNode* root, vector<int> &ans){
+    void rightBoundary(TreeNode* root){
         if (!root || (root->left == NULL && root->right == NULL)) return;
-        rightBoundary((root->right ? root->right: root->left), ans);
+        rightBoundary(root->right ? root->right: root->left);
         ans.push_back(root->val);
     }
     vector<int> boundaryOfBinaryTree(TreeNode* root) {
-        vector<int> ans;
         if(!root) return ans;
         ans.push_back(root->val);
-        leftBoundary(root->left, ans);
-        if((root->left != NULL || root->right != NULL)) leaves(root, ans);
-        rightBoundary(root->right, ans);
+        leftBoundary(root->left);
+        if((root->left != NULL || root->right != NULL)) leaves(root);
+        rightBoundary(root->right);
         return ans;
     }
 };
