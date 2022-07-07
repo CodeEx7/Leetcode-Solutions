c52becf3e3b627f5fe402937b1a5f3ae05b87f24
Brij Bhushan Maurya <62421744+Strange1-7@users.noreply.github.com>
2022-07-07 19:25:38 +0530

    Time: 38 ms (28.43%), Space: 31.2 MB (17.60%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -1,37 +1,27 @@
-class Solution
-{
-    public:
-        unordered_map<string, bool> mem;
-    bool check(string s1, string s2, string s3, int len1, int len2, int len3, int p1, int p2, int p3)
-    {
-        if (p3 == len3)
-            return (p1 == len1 and p2 == len2) ? true : false;
-
-        string key = to_string(p1) + "*" + to_string(p2) + "*" + to_string(p3);
-        if (mem.find(key) != mem.end())
-            return mem[key];
-
-        if (p1 == len1)
-            return mem[key] = s2[p2] == s3[p3] ? check(s1, s2, s3, len1, len2, len3, p1, p2 + 1, p3 + 1) : false;
-        if (p2 == len2)
-            return mem[key] = s1[p1] == s3[p3] ? check(s1, s2, s3, len1, len2, len3, p1 + 1, p2, p3 + 1) : false;
-
-        bool one = false, two = false;
-        if (s1[p1] == s3[p3])
-            one = check(s1, s2, s3, len1, len2, len3, p1 + 1, p2, p3 + 1);
-        if (s2[p2] == s3[p3])
-            two = check(s1, s2, s3, len1, len2, len3, p1, p2 + 1, p3 + 1);
-
-        return mem[key] = one or two;
+class Solution {
+public:
+    //helper function
+    bool solve(string s1, string s2, string s3, int n1, int n2, int n3, vector<vector<int>> &dp){
+        if(n3==0) return 1;
+        if(dp[n1][n2]!=-1) return dp[n1][n2];
+        
+        int a,b;
+            a=b=0;
+        
+        if(n1-1>=0 && s1[n1-1]==s3[n3-1]) a=solve(s1,s2,s3,n1-1, n2, n3-1, dp);
+        if(n2-1>=0 && s2[n2-1]==s3[n3-1]) b=solve(s1,s2,s3,n1,n2-1, n3-1, dp);
+        
+        return dp[n1][n2]= a or b;
+        
     }
-    bool isInterleave(string s1, string s2, string s3)
-    {
-        int len1 = s1.length();
-        int len2 = s2.length();
-        int len3 = s3.length();
-        if (len3 != len1 + len2)
-            return false;
-
-        return check(s1, s2, s3, len1, len2, len3, 0, 0, 0);
+    //main working function
+    bool isInterleave(string s1, string s2, string s3) {
+        int n1=s1.size(), n2=s2.size(), n3=s3.size();
+        
+        if(n3!=(n1+n2)) return false;
+        
+        vector<vector<int>> dp(101, vector<int> (101, -1));
+        
+        return solve(s1,s2,s3,n1,n2,n3, dp);
     }
 };
