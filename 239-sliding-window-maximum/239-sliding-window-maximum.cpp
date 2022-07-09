727552d13171402a23e99ed5924d0c19ea4d1b06
Brij Bhushan Maurya <62421744+Strange1-7@users.noreply.github.com>
2022-07-09 18:03:07 +0530

    Time: 619 ms (22.44%), Space: 148.6 MB (29.72%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/239-sliding-window-maximum.cpp
@@ -1,30 +1,21 @@
 class Solution {
 public:
-    vector<int> maxSlidingWindow(vector<int>& arr, int k) {
-        // create a priority queue (Max Heap) of pair of int
-        // first of the pair is the element of the array
-        // and second is the index
-        priority_queue<pair<int,int>> p;
-        // create a vector to store the result
-        vector<int>res;
-        // add all the elements in the first window to the PQ
-        for(int i = 0; i < k; i++)
-            p.push({arr[i],i});
-        // add the max element of first window to the res vector
-        res.push_back(p.top().first);
-        // start iteration from the second window
-        for(int i = k; i < arr.size(); i++)
-        {
-            // add the element into the PQ
-            p.push({arr[i],i});
-            // remove all the elments which are not in the current window
-            // current window is from [i-k+1 , i]
-            while((p.top().second <= i-k))
-                p.pop();
-            // add the max element in the PQ to the res
-            res.push_back(p.top().first);
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        vector<int> res;
+        priority_queue<pair<int,int>> pq;
+        
+        for(int i=0;i<k;i++){
+            pq.push({nums[i],i});
         }
-        // return the result
+        res.push_back(pq.top().first);
+        
+        for(int i=k;i<nums.size();i++){
+            pq.push({nums[i],i});
+            
+            while(!(pq.top().second > (i-k))) pq.pop();
+            
+            res.push_back(pq.top().first);
+        }
         return res;
     }
 };
