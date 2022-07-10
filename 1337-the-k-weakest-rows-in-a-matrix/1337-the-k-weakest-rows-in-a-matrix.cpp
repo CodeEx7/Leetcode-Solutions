c44b585fd61d7f80ce6d2755c3b3c5fb1c576dc4
Brij Bhushan Maurya <62421744+Strange1-7@users.noreply.github.com>
2022-07-11 01:35:42 +0530

    Time: 25 ms (24.29%), Space: 10.5 MB (41.15%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.cpp
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.cpp
@@ -5,7 +5,7 @@ public:
         for(int i=0;i<mat.size();i++){
             mat[i].push_back(i);
         }
-        sort(mat.begin(), mat.end());
+        stable_sort(mat.begin(), mat.end());
         
         vector<int> res(k);
         for(int i=0;i<k;i++){
