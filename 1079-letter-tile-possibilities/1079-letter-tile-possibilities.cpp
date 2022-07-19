5d57ab6197f0a01d62a6f51f80be00f22ba4bb92
Brij Bhushan Maurya <62421744+Strange1-7@users.noreply.github.com>
2022-07-20 01:36:57 +0530

    Time: 4 ms (94.20%), Space: 6 MB (91.33%) - LeetHub

--- 1079-letter-tile-possibilities/1079-letter-tile-possibilities.cpp
+++ 1079-letter-tile-possibilities/1079-letter-tile-possibilities.cpp
@@ -1,3 +1,14 @@
+/*
+                    h( AAB , -1 , 0 )              LEVEL=0 ie 0 length substring
+                    /               \
+            h('A'AB,0,1)           h('B'AA,6,1)    LEVEL=1 ie 1 length substrings ("A","B")
+            /         \              /
+        h('AA'B,1,2) h('AB'A,4,2) h('BA'A,7,2)     LEVEL=2 ie 2 length substrings ("AB","BA","AA")
+        /            /             /
+    h('AAB',2,3)    h('ABA',5,2)  h('BAA',8,3)     LEVEL=3 ie 3 length substrings 
+                                                   ("AAB", "ABA", "BAA")
+*/
+
 class Solution {
 public:
     //helper function
