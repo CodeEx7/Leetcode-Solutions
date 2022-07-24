f7d9342e6fae8634f8ab4f5f6ec6d0f1d10fefdc
Brij Bhushan Maurya <62421744+Strange1-7@users.noreply.github.com>
2022-07-24 22:15:49 +0530

    Time: 16 ms (50.49%), Space: 11.2 MB (68.79%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -2,21 +2,35 @@ class Solution {
 public:
     //helper function
     void helper(vector<int>& candidates, int target, vector<vector<int>> &res, vector<int> &combination, int begin){
-        if(target==0){
-            res.push_back(combination);
+        
+        if(begin==candidates.size()){
+            if(target==0){
+                res.push_back(combination);
+            }
             return;
         }
-        for(int i=begin;i<candidates.size() && target>=candidates[i];i++){
-            combination.push_back(candidates[i]);
-            helper(candidates, target-candidates[i], res, combination, i);
+        //picking up the element
+        if(target>=candidates[begin]){
+            combination.push_back(candidates[begin]);
+            helper(candidates, target-candidates[begin], res, combination, begin);
             combination.pop_back();
         }
+        helper(candidates, target, res, combination, begin+1);
+        // if(target==0){
+        //     res.push_back(combination);
+        //     return;
+        // }
+        // for(int i=begin;i<candidates.size() && target>=candidates[i];i++){
+        //     combination.push_back(candidates[i]);
+        //     helper(candidates, target-candidates[i], res, combination, i);
+        //     combination.pop_back();
+        // }
     }
     //main working function
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<vector<int>> res;
         vector<int> combination;
-        sort(candidates.begin(), candidates.end());
+        //sort(candidates.begin(), candidates.end());
         helper(candidates, target, res, combination, 0);
         return res;
     }
