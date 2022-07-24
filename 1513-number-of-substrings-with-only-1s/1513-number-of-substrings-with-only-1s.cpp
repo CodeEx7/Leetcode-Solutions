28c6b16640f5e52ea35ce4ed5ebdd584af151f28
Brij Bhushan Maurya <62421744+Strange1-7@users.noreply.github.com>
2022-07-25 02:35:11 +0530

    Time: 26 ms (9.44%), Space: 8.5 MB (99.52%) - LeetHub

---
+++ 1513-number-of-substrings-with-only-1s/1513-number-of-substrings-with-only-1s.cpp
@@ -0,0 +1,22 @@
+#define ll long long
+class Solution {
+public:
+    int numSub(string s) {
+        ll ans=0;
+        ll cnt=0;
+        for( auto ch:s){
+            if(ch=='1'){
+                cnt++;
+            }else{
+                ans+=(cnt*(cnt+1)/2);
+                    ans=ans%1000000007;
+                cnt=0;
+            }
+        }
+        if(cnt!=0){
+            ans+=(cnt*(cnt+1)/2);
+            ans=ans%1000000007;
+        }
+        return ans;
+    }
+};
