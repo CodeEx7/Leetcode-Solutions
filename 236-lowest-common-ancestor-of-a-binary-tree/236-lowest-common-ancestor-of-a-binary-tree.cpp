d30b2908cc06aaae0728ec34e1e9ff72dcfde458
Brij Bhushan Maurya <62421744+Strange1-7@users.noreply.github.com>
2022-07-26 12:58:57 +0530

    Time: 20 ms (73.61%), Space: 14.2 MB (85.91%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -10,42 +10,24 @@
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-    stack<TreeNode*> path_p, path_q;
-    get_path_to_node(root, p, path_p);
-    get_path_to_node(root, q, path_q);
-    
-    return lca_from_paths(path_p, path_q);
-  }
-  
-private:
-  bool get_path_to_node(TreeNode* root, TreeNode* node, 
-      stack<TreeNode*>& path) {
-    if (!root) {
-      return false;
+        if(root==NULL) return NULL;
+        if(root==p || root==q) return root;
+        
+        TreeNode *left=NULL, *right=NULL;
+        
+        if(root->left){
+            left=lowestCommonAncestor(root->left, p, q);
+        }
+        if(root->right){
+            right=lowestCommonAncestor(root->right, p, q);
+        }
+        if(left!=NULL && right!=NULL) return root;
+        
+        if(left==NULL){
+            return right;
+        }else{
+            return left;
+        }
+        return NULL;
     }
-    
-    if (root == node || 
-        get_path_to_node(root->left, node, path) ||
-        get_path_to_node(root->right, node, path)) {
-      path.push(root);
-      return true;
-    }
-    
-    return false;
-  }
-  
-  TreeNode* lca_from_paths(stack<TreeNode*>& path1,
-      stack<TreeNode*>& path2) {    
-    TreeNode* lca = NULL;
-    while (!path1.empty() && !path2.empty()) {
-      if (path1.top() != path2.top()) {
-        break;
-      }
-      
-      lca = path1.top();
-      path1.pop(), path2.pop();
-    }
-    
-    return lca;
-  }
 };
