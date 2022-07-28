6c7b0a4c71725d047884fa32ad5335956b61770c
Brij Bhushan Maurya <62421744+Strange1-7@users.noreply.github.com>
2022-07-28 15:00:42 +0530

    Time: 18 ms (40.58%), Space: 7.2 MB (94.64%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -2,10 +2,16 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
         if(s.size()!=t.size()) return false;
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
+        
+        unordered_map<char, int> mapp;
+        
         for(int i=0;i<s.size();i++){
-            if(s[i]!=t[i]) return false;
+            mapp[s[i]-'a']++;
+            mapp[t[i]-'a']--;
+        }
+        
+        for(auto it:mapp){
+            if(it.second!=0) return false;
         }
         return true;
     }
