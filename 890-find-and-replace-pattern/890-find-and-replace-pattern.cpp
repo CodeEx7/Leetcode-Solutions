a9967acbae521f4d4b84f0c57561691604249311
Brij Bhushan Maurya <62421744+Strange1-7@users.noreply.github.com>
2022-07-29 14:09:12 +0530

    Time: 8 ms (44.61%), Space: 8.4 MB (53.91%) - LeetHub

--- 890-find-and-replace-pattern/890-find-and-replace-pattern.cpp
+++ 890-find-and-replace-pattern/890-find-and-replace-pattern.cpp
@@ -1,32 +1,18 @@
 class Solution {
 public:
-    
-    bool check(string &word, string &pattern){
-        unordered_map<char, char> w2p;
-        unordered_map<char, char> p2w;
-        for(int i=0;i<word.size();i++){
-            if(w2p.find(word[i])!=w2p.end()){
-                if(w2p[word[i]]!=pattern[i]) return false;
-            }else{
-                w2p[word[i]]=pattern[i];
-                }
-            if(p2w.find(pattern[i])!=p2w.end()){
-                if(p2w[pattern[i]]!=word[i]) return false;
-            }else{
-                p2w[pattern[i]]=word[i];
-                }
-            }
-        return true;
-    }
-    vector<string> findAndReplacePattern(vector<string>& words, string pattern) {
-        vector<string> res;   
-        for(auto word:words){
-            if(word.size()==pattern.size()){
-                if(check(word, pattern)==true){
-                    res.push_back(word);
-                }
-            }
-        }
-        return res;
-    }
+   string toPattern(string word) {
+    map<char, char> M;
+    int curr = 'a';
+    for(char& c: word) if(M.find(c)==M.end()) M[c] = (char) curr++;
+    for(int i = 0; i < word.length(); i++) word[i] = M[word[i]];
+       cout<<word<<" ";
+    return word;
+  }
+  
+  vector<string> findAndReplacePattern(vector<string>& words, string pattern) {
+    string p = toPattern(pattern);
+    vector<string> res;
+    for(string& w: words) if(toPattern(w).compare(p) == 0) res.push_back(w);
+    return res;
+  }
 };
