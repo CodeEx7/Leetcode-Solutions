02d82894ed5d71ae0dfd9162d670e49a483edb65
Brij Bhushan Maurya <62421744+Strange1-7@users.noreply.github.com>
2022-07-30 02:38:04 +0530

    Time: 19 ms (99.28%), Space: 13.7 MB (42.32%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.cpp
+++ 785-is-graph-bipartite/785-is-graph-bipartite.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
     //BFS function
-    bool BFS(int s, vector<int> &color, vector<vector<int>> &graph){
+    bool BFS(int s, vector<int> &color, vector<bool> &vis, vector<vector<int>> &graph){
         queue<int> q;
         q.push(s);
-        color[s]=1;
+        color[s]=0;
+        vis[s]=true;
         
         while(!q.empty()){
             int u=q.front();
             q.pop();
             for(auto v:graph[u]){
-                if(color[v]==-1){
+                if(vis[v]==false){
                     q.push(v);
-                    color[v]=1-color[u];
+                    vis[v]=true;
+                    if(color[u]==0) color[v]=1;
+                    else color[v]=0;
                 } else if(color[v]==color[u]){
                     return false;
                 }
@@ -23,11 +26,12 @@ public:
     //main working functioon
     bool isBipartite(vector<vector<int>>& graph) {
         int V=graph.size();
-        vector<int> color(V+1, -1);
+        vector<int> color(V+1);
+        vector<bool> vis(V+1, false);
         
         for(int i=0;i<V;i++){
-            if(color[i]==-1){
-                if(BFS(i, color, graph)==false) return false;
+            if(vis[i]==false){
+                if(BFS(i, color, vis, graph)==false) return false;
             }
         }
         return true;
