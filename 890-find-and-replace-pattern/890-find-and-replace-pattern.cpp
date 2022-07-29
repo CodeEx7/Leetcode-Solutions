8ac644122c9b28481db9d12277cecddcdc18f07f
Brij Bhushan Maurya <62421744+Strange1-7@users.noreply.github.com>
2022-07-29 14:40:52 +0530

    Time: 6 ms (62.68%), Space: 8.4 MB (53.91%) - LeetHub

--- 890-find-and-replace-pattern/890-find-and-replace-pattern.cpp
+++ 890-find-and-replace-pattern/890-find-and-replace-pattern.cpp
@@ -1,18 +1,28 @@
 class Solution {
 public:
-   string toPattern(string word) {
-    map<char, char> M;
-    int curr = 'a';
-    for(char& c: word) if(M.find(c)==M.end()) M[c] = (char) curr++;
-    for(int i = 0; i < word.length(); i++) word[i] = M[word[i]];
-       cout<<word<<" ";
-    return word;
-  }
-  
-  vector<string> findAndReplacePattern(vector<string>& words, string pattern) {
-    string p = toPattern(pattern);
-    vector<string> res;
-    for(string& w: words) if(toPattern(w).compare(p) == 0) res.push_back(w);
-    return res;
-  }
+    //normalise function
+    string normalise(string word){
+        char c='a';
+        unordered_map<char, char> mapp;
+        for(auto ch:word){
+            if(mapp.find(ch)==mapp.end()){
+                mapp[ch] = c++;
+            }
+        }
+        for(int i=0;i<word.size();i++){
+            word[i]=mapp[word[i]];
+        }
+        return word;
+    }
+    //main working function
+    vector<string> findAndReplacePattern(vector<string>& words, string pattern) {
+        vector<string> res;
+        string norm_pattern=normalise(pattern);
+        for(auto word:words){
+            if(normalise(word)==norm_pattern){
+                res.push_back(word);
+            }
+        }
+        return res;
+    }
 };
