bc42f88b877ff497ec69d36f780cd4a87a538d60
Brij Bhushan Maurya <62421744+CodeEx7@users.noreply.github.com>
2022-08-03 00:58:13 +0530

    Time: 7 ms (79.55%), Space: 9.1 MB (7.98%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
+    int helper(int i, int j, int n, vector<vector<int>> &tringle,vector<vector<int>> &memo){
+        if(i==n-1) return tringle[i][j];
+        if(memo[i][j]!=-1) return memo[i][j];
+        return memo[i][j]=tringle[i][j]+min(helper(i+1,j,n,tringle, memo), helper(i+1,j+1,n,tringle, memo));
+    }
+    
+    
     int minimumTotal(vector<vector<int>>& triangle) {
         int n=triangle.size();
-        vector<int> dp(n, 0);
-        for(int i=0;i<triangle[n-1].size();i++){
-            dp[i]=triangle[n-1][i];
-        }
-        
-        for(int i=n-2;i>=0;i--){
-            for(int j=0;j<triangle[i].size();j++){
-                dp[j]=min(dp[j], dp[j+1])+triangle[i][j];
-            }
-        }
-        return dp[0];
+        vector<vector<int>> memo(n,vector<int>(n,-1));
+        return helper(0,0,n, triangle, memo);
     }
 };
