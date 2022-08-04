3e0387f4932f393a234780ae0e556d5fcae716c0
Brij Bhushan Maurya <62421744+CodeEx7@users.noreply.github.com>
2022-08-04 23:04:42 +0530

    Time: 3 ms (88.66%), Space: 11.2 MB (29.09%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -1,24 +1,47 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-        int start=0, end=nums.size()-1;
-        while(start <= end){
-            int mid=start+(end-start)/2;
-            
-            if(nums[mid]==target) return mid;
-            
-            if(nums[mid] >= nums[start]){
-                if(target >= nums[start] && target <= nums[mid])
-                    end=mid-1;
-                else
-                    start=mid+1;
-            }else{
-                if(target > nums[mid] && target <= nums[end])
-                    start=mid+1;
-                else
-                    end=mid-1;
-            }
-        }
-        return -1;
+    int findPivot(vector<int>& nums){
+    int s=0, e=nums.size()-1, mid=s+(e-s)/2;
+    
+    while(s<e){
+        if(nums[mid]>=nums[0])
+            s=mid+1;
+        
+        else
+            e=mid;
+        
+        mid=s+(e-s)/2;
     }
+    return s;
+}
+
+int binarySearch(vector<int>& nums, int s, int e, int target){
+    int mid = s+(e-s)/2;
+    
+    while(s<=e){
+        if(nums[mid]==target)
+            return mid;
+        
+        else if(nums[mid]<target)
+            s=mid+1;
+        
+        else
+            e=mid-1;
+        
+        mid = s+(e-s)/2;
+    }
+    return -1;
+}
+
+int search(vector<int>& nums, int target) {
+    int pivot = findPivot(nums);
+    int answer;
+    
+    if(nums[pivot]<=target && target<=nums[nums.size()-1])
+        answer = binarySearch(nums, pivot, nums.size()-1, target);
+    else
+        answer = binarySearch(nums, 0, pivot-1, target);
+    
+    return answer;
+}
 };
