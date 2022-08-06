419c3dfda7be76e6a8254cdcc51dca5c1453d355
Brij Bhushan Maurya <62421744+CodeEx7@users.noreply.github.com>
2022-08-06 16:54:11 +0530

    Time: 4 ms (91.04%), Space: 9 MB (32.98%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.cpp
+++ 496-next-greater-element-i/496-next-greater-element-i.cpp
@@ -1,21 +1,36 @@
 class Solution {
 public:
-    int find_next(vector<int> &nums, int idx, int target){
-        for(int i=idx;i<nums.size();i++){
-            if(nums[i]>target){
-                return nums[i];
+    //finding next greater element
+    void findNGE(vector<int> nums, vector<int> &res){
+        stack<int> st;
+        int n=nums.size();
+        for(int i=n-1;i>=0;i--){
+            if(st.empty()) res.push_back(-1);
+            else if(st.size()>0 and st.top()>nums[i]){
+                res.push_back(st.top());
+            }else if(st.size()>0 && st.top()<=nums[i]){
+                while(st.size()>0 && st.top()<=nums[i]){
+                    st.pop();
+                }
+                if(st.empty()) res.push_back(-1);
+                else if(st.size()>0 and st.top()>nums[i]){
+                res.push_back(st.top());
             }
         }
-        return -1;
+            st.push(nums[i]);
     }
+   reverse(res.begin(), res.end());             
+}
+    //main working function
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        vector<int> res;
-        for(int i=0;i<nums1.size();i++){
-            for(int j=0;j<nums2.size();j++){
-                if(nums1[i]==nums2[j]){
-                    res.push_back(find_next(nums2, j, nums2[j]));
-                }
-            }
+        vector<int> NGE, res;
+        findNGE(nums2,NGE);
+        unordered_map<int,int> mapp;
+        for(int i=0;i<nums2.size();i++){
+            mapp[nums2[i]]=i;
+        }
+        for(auto num:nums1){
+            res.push_back(NGE[mapp[num]]);
         }
         return res;
     }
