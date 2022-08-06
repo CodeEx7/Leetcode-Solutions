a9ca585675c708cbbe28e02e5719b15417653556
Brij Bhushan Maurya <62421744+CodeEx7@users.noreply.github.com>
2022-08-07 01:29:57 +0530

    Time: 408 ms (100.00%), Space: 102.2 MB (100.00%) - LeetHub

--- 2365-task-scheduler-ii/2365-task-scheduler-ii.cpp
+++ 2365-task-scheduler-ii/2365-task-scheduler-ii.cpp
@@ -7,16 +7,13 @@ public:
             if(mapp.find(nums[i])==mapp.end()){
                 day++;
                 mapp[nums[i]]=day;
-                //cout<<i<<" "<<day<<endl;
             }else{
                 if(day-mapp[nums[i]]<space){
                     day+=space-(day-mapp[nums[i]])+1;
                     mapp[nums[i]]=day;
-                    //cout<<i<<" "<<day<<endl;
                 }else{
                     day++;
                     mapp[nums[i]]=day;
-                    //cout<<i<<" "<<day<<endl;
                 }
             }
         }
