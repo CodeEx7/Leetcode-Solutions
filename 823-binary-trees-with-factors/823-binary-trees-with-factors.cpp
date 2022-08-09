3c9f5b759cd0c829534591b6fe5e775b8bf62fb1
Brij Bhushan Maurya <62421744+CodeEx7@users.noreply.github.com>
2022-08-09 11:01:12 +0530

    Time: 84 ms (75.38%), Space: 9.1 MB (91.08%) - LeetHub

---
+++ 823-binary-trees-with-factors/823-binary-trees-with-factors.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int numFactoredBinaryTrees(vector<int>& arr) {
+        long long int mod=pow(10,9)+7;
+        unordered_map<int,long long int> mapp;
+        for(auto num:arr){
+            mapp[num]=1;
+        }
+        sort(arr.begin(), arr.end());
+        for(int i=1;i<arr.size();i++){
+            long long int cnt=0;
+            for(int j=0;j<i;j++){
+                if(arr[i]%arr[j]==0){
+                    if(mapp.find(arr[i]/arr[j])!=mapp.end()){
+                        cnt+=(mapp[arr[j]]*mapp[arr[i]/arr[j]]);
+                    }
+                }
+            }
+            mapp[arr[i]]+=cnt;
+            mapp[arr[i]]%=mod;
+        }
+        long long int res=0;
+        for(auto it:mapp){
+            res+=it.second;
+        }
+        return res%mod;
+    }
+};
