7f678151fa3dfe3ca514751028d602ca316c0e71
Brij Bhushan Maurya <62421744+CodeEx7@users.noreply.github.com>
2022-08-19 23:57:07 +0530

    Time: 11 ms (93.47%), Space: 12.8 MB (34.74%) - LeetHub

--- 692-top-k-frequent-words/692-top-k-frequent-words.cpp
+++ 692-top-k-frequent-words/692-top-k-frequent-words.cpp
@@ -2,11 +2,8 @@ class Solution {
 public:
     //mycmp function
     static bool mycmp(const pair<int, string> &p1, const pair<int, string> &p2){
-        if(p1.first>p2.first)
+        if(p1.first>p2.first ||(p1.first==p2.first && p1.second<p2.second))
             return true;
-        else if(p1.first==p2.first && p1.second<p2.second){
-            return true;
-        }
         return false;
     }
     //main working function
