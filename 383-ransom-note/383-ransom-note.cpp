1dec3d1e7203ba081df3636ca482c194d66570bd
Brij Bhushan Maurya <62421744+CodeEx7@users.noreply.github.com>
2022-08-25 10:08:38 +0530

    Time: 34 ms (35.87%), Space: 8.8 MB (39.49%) - LeetHub

--- 383-ransom-note/383-ransom-note.cpp
+++ 383-ransom-note/383-ransom-note.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
-     map<char,int>m1,m2;
-        for(int i=0;i<ransomNote.size();i++)
-        {
-            m1[ransomNote[i]]++;
+       unordered_map<char,int> mapp;
+        for(auto ch:magazine){
+            mapp[ch]++;
         }
-        for(int i=0;i<magazine.size();i++)
-        {
-            m2[magazine[i]]++;
-        }
-        for(auto it:m1)
-        {
-            if(m2.find(it.first)==m2.end()||m2[it.first]<it.second)
+        
+        for(auto ch:ransomNote){
+            if(mapp.find(ch)==mapp.end()){
                 return false;
+            }else{
+                if(mapp[ch]<=0){
+                    return false;
+                }else{
+                    mapp[ch]--;
+                }
+            }
         }
         return true;
     }
