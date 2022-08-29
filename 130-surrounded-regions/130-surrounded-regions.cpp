5c5ab5f656b53192f287983fbf0edbf58f31780a
Brij Bhushan Maurya <62421744+CodeEx7@users.noreply.github.com>
2022-08-29 10:43:37 +0530

    Time: 60 ms (5.33%), Space: 10 MB (48.58%) - LeetHub

--- 130-surrounded-regions/130-surrounded-regions.cpp
+++ 130-surrounded-regions/130-surrounded-regions.cpp
@@ -17,41 +17,25 @@ public:
     //main working function
     void solve(vector<vector<char>>& board) {
          int m=board.size(), n=board[0].size();
-    //     //for colums check
-    //     for(int i=0;i<m;i++){
-    //         if(board[i][0]=='O'){
-    //             DFS(board,i,0);
-    //         }
-    //         if(board[i][n-1]=='O'){
-    //             DFS(board,i,n-1);
-    //         }
-    //     }
-    //     //for rows check
-    //     for(int j=0;j<n;j++){
-    //         if(board[0][j]=='O'){
-    //             DFS(board,0,j);
-    //         }
-    //         if(board[0][m-1]=='O'){
-    //             DFS(board,m-1,j);
-    //         }
-    //     }
-    
-    //Moving over firts and last column   
-     for(int i=0; i<m; i++) {
-         if(board[i][0] == 'O')
-             DFS(board, i, 0);
-         if(board[i][n-1] == 'O')
-             DFS(board, i, n-1);
-     }
+        //for colums check
+        for(int i=0;i<m;i++){
+            if(board[i][0]=='O'){
+                DFS(board,i,0);
+            }
+            if(board[i][n-1]=='O'){
+                DFS(board,i,n-1);
+            }
+        }
+        //for rows check
+        for(int j=0;j<n;j++){
+            if(board[0][j]=='O'){
+                DFS(board,0,j);
+            }
+            if(board[m-1][j]=='O'){
+                DFS(board,m-1,j);
+            }
+        }
         
-        
-     //Moving over first and last row   
-     for(int j=0; j<n; j++) {
-         if(board[0][j] == 'O')
-             DFS(board, 0, j);
-         if(board[m-1][j] == 'O')
-             DFS(board, m-1, j);
-     }
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 cout<<board[i][j]<<" ";
