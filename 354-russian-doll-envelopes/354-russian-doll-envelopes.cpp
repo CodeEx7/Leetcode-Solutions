e02c1e8ca6284379b8ec54d85f0c601975d47b8b
Brij Bhushan Maurya <62421744+CodeEx7@users.noreply.github.com>
2022-09-09 15:17:13 +0530

    Time: 838 ms (11.64%), Space: 77.6 MB (67.39%) - LeetHub

--- 354-russian-doll-envelopes/354-russian-doll-envelopes.cpp
+++ 354-russian-doll-envelopes/354-russian-doll-envelopes.cpp
@@ -1,41 +1,40 @@
 class Solution {
 public:
-    static bool my_comp(vector<int> &a, vector<int> &b){
-        if (a[0] == b[0]) {
-            return (a[1] > b[1]);
+    //--------------------------------------------
+    static bool mycomp(vector<int> &a, vector<int> &b){
+        if(a[0]==b[0]){
+            return a[1]>b[1];
         }
-        return a[0] < b[0];
+        return a[0]<b[0];
     }
-    
-    int maxEnvelopes(vector<vector<int>>& envelopes) {
-        /* Sort envelopes in increasing order of width and decreasing order of height.
-        *  Then, apply LIS on heights.
-        *    LIS:
-        *    Create a tmp_arr to save sequence.
-        *    If curr_elem in arr is greater than tmp_arr.back(), append curr_elem to tmp_arr.
-        *    Else, replace elem in tmp_arr such that,
-        *      curr_elem < tmp_elem and it should not break incerasing nature of tmp_arr.
-        *      (Binary search for curr_elem in tmp_arr will give you the proper index)
-        *    Finally return len of tmp_arr.
-        */
-        
-        sort(envelopes.begin(), envelopes.end(), my_comp);
-
-        vector<int> tmp;
-        tmp.push_back(envelopes[0][1]);
-        for (int i = 1; i < envelopes.size(); i++){
-            int curr_elem = envelopes[i][1];
-            if (tmp.back() < curr_elem)
-            {
-                tmp.push_back(curr_elem);
+    //---------------------------------------------
+    int ceilIdx(vector<int> &arr, int target){
+        int left=0, right=arr.size()-1;
+        while(left<right){
+            int mid=left+(right-left)/2;
+            if(arr[mid]>=target){
+                right=mid;
+            }else{
+                left=mid+1;
             }
-            else 
-            {
-                int idx = lower_bound(tmp.begin(), tmp.end(), curr_elem) - tmp.begin();
-                tmp[idx] = curr_elem;
-            }
-            
         }
-        return tmp.size();
+        return right;
+    }
+    //---------------------------------------------
+    int maxEnvelopes(vector<vector<int>>& envelopes) {
+        sort(envelopes.begin(), envelopes.end(), mycomp);
+        
+        //finding out LIS according to second value
+        vector<int> tail;
+        tail.emplace_back(envelopes[0][1]);
+        for(int i=1;i<envelopes.size();i++){
+            if(envelopes[i][1]>tail.back()){
+                tail.emplace_back(envelopes[i][1]);
+            }else{
+                int idx=ceilIdx(tail, envelopes[i][1]);
+                tail[idx]=envelopes[i][1];
+            }
+        }
+        return tail.size();
     }
 };
