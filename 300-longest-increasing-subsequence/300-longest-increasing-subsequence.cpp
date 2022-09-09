900bd135674c5e8b07782d810317634f5106cee6
Brij Bhushan Maurya <62421744+CodeEx7@users.noreply.github.com>
2022-09-09 13:39:07 +0530

    Time: 16 ms (76.18%), Space: 10.5 MB (67.26%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,17 +1,29 @@
 class Solution {
 public:
-    int lengthOfLIS(vector<int>& nums) {
-        vector<int> lis(nums.size(),1);
-        
-        for(int i=0;i<nums.size();i++){
-            for(int j=0;j<i;j++){
-                if(nums[i]>nums[j]){
-                    lis[i]=max(lis[i],lis[j]+1);
-                }
+    int ceilIdx(vector<int> &tail, int target){
+        int left=0, right=tail.size()-1;
+        while(left<right){
+            int mid=left+(right-left)/2;
+            if(tail[mid]>=target){
+                right=mid;
+            }else{
+                left=mid+1;
             }
         }
-        return *max_element(lis.begin(), lis.end());
+        return right;
+    }
+    //--------------------------------
+    int lengthOfLIS(vector<int>& nums) {
+        vector<int> tail;
+        tail.push_back(nums[0]);
+        for(int i=1;i<nums.size();i++){
+            if(nums[i]>tail.back()){
+                tail.push_back(nums[i]);
+            }else{
+                int idx=ceilIdx(tail, nums[i]);
+                tail[idx]=nums[i];
+            }
+        }
+        return tail.size();
     }
 };
-
-//TC==> O(n^2)
