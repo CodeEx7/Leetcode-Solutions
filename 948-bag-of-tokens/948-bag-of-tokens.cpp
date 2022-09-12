c54efd4b89524f78cc34baf56a8249085eedfa26
Brij Bhushan Maurya <62421744+CodeEx7@users.noreply.github.com>
2022-09-12 12:15:17 +0530

    Time: 17 ms (15.52%), Space: 10.6 MB (66.86%) - LeetHub

--- 948-bag-of-tokens/948-bag-of-tokens.cpp
+++ 948-bag-of-tokens/948-bag-of-tokens.cpp
@@ -1,33 +1,23 @@
 class Solution {
 public:
     int bagOfTokensScore(vector<int>& tokens, int power) {
-        if(tokens.size()==0) return 0;
-        if(tokens.size()==1){
-            if(power>tokens[0]){
-                return 1;
-            }else{
-                return 0;
-            }
-        }
         sort(tokens.begin(), tokens.end());
         //--------------------
         int res=0, curr_power=0;
         int left=0, right=tokens.size()-1;
-        if(tokens[left]<=power){
         while(left<=right){
             if(tokens[left]<=power){
                 curr_power++;
                 power-=tokens[left];
-                cout<<power<<" ";
                 left++;
                 res=max(res, curr_power);
             }else if(curr_power>=1){
                 curr_power--;
                 power+=tokens[right];
-                cout<<power<<" ";
                 right--;
+            }else{
+                break;
             }
-        }
         }
         return res;
     }
