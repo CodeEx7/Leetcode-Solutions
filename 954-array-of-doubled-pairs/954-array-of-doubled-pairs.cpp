3126cefc5356d677dcac5981fd87135bc4e848e7
Brij Bhushan Maurya <62421744+CodeEx7@users.noreply.github.com>
2022-09-15 16:46:33 +0530

    Time: 222 ms (46.41%), Space: 61.4 MB (14.69%) - LeetHub

--- 954-array-of-doubled-pairs/954-array-of-doubled-pairs.cpp
+++ 954-array-of-doubled-pairs/954-array-of-doubled-pairs.cpp
@@ -1,33 +1,28 @@
 class Solution {
-public:
+public:  
+    
     static bool compare(int a,int b){
         return abs(a)<abs(b);
     }
     
     bool canReorderDoubled(vector<int>& arr) {
-        if(arr.size()%2!=0) return false;
+        if(arr.size()%2!=0) return {};
+        vector<int> res;
         
         sort(arr.begin(), arr.end(), compare);
         
         unordered_map<int,int> mapp;
-        for(auto ele:arr){
-            mapp[ele]++;
+        for(int i=0;i<arr.size();i++){
+            mapp[arr[i]]++;
         }
         
         for(int i=0;i<arr.size();i++){
-            if(mapp[arr[i]]==0)  continue;
-            if(mapp[arr[i]*2]==0) return false;
-                mapp[arr[i]]--;
-                mapp[2*arr[i]]--;
-            
+            if (mapp[arr[i]] == 0) continue;
+            if (mapp[arr[i] * 2] == 0) return {};
+            res.push_back(arr[i]);
+            mapp[arr[i]]--;
+            mapp[arr[i] * 2]--;
         }
-        // for(auto it:mapp){
-        //     cout<<it.first<<" "<<it.second<<endl;
-        // }
-        // for(int i=0;i<arr.size();i++){
-        //     if(mapp[arr[i]]!=0)
-        //         return false;
-        // }
-        return true;
+        return res.size()==arr.size()/2;
     }
 };
