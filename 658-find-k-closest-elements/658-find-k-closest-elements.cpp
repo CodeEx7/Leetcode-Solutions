f81d6ddfdf96acbadb916c4981a2be0635381afd
Brij Bhushan Maurya <62421744+CodeEx7@users.noreply.github.com>
2022-09-29 11:10:27 +0530

    Time: 154 ms (28.34%), Space: 31.7 MB (71.10%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.cpp
+++ 658-find-k-closest-elements/658-find-k-closest-elements.cpp
@@ -1,27 +1,19 @@
-class cmp {
-    int param;
-public:
-    cmp(int x) : param(x) {}
-
-    bool operator()(int a, int b) {
-        if(abs(a-param)==abs(b-param)){
-            return a<b;
-        }
-        return (abs(a-param)<abs(b-param));
-    }
-};
-//main ----------------------------
 class Solution {
 public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+       sort(arr.begin(), arr.end(), [x](const auto a, const auto b){
+            if(abs(a-x)==abs(b-x)){
+                return a<b;
+            }
+           return (abs(a-x)<abs(b-x));
+        });
+        
         vector<int> res;
-        
-        sort(arr.begin(), arr.end(), cmp(x));
-        
         for(int i=0;i<k;i++){
             res.emplace_back(arr[i]);
         }
-        sort(res.begin(), res.end());
-        return res;
+        
+        sort(res.begin(), res.end());          
+        return res; 
     }
 };
