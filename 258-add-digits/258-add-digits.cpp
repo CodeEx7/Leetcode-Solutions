7440a3f2c455d1eed03f80a65abec1dd4dc7e953
Brij Bhushan Maurya <62421744+CodeEx7@users.noreply.github.com>
2022-10-03 15:26:41 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (94.60%) - LeetHub

--- 258-add-digits/258-add-digits.cpp
+++ 258-add-digits/258-add-digits.cpp
@@ -1,17 +1,13 @@
 class Solution {
 public:
-    int sumDigits(int num){
-        int res=0;
-        while(num>0){
-            res+=num%10;
-            num=num/10;
-        }
-        return res;
-    }
-    //--------------
     int addDigits(int num) {        
         while(num>9){
-            num=sumDigits(num);
+            int res=0;
+            while(num>0){
+                res+=num%10;
+                num=num/10;
+            }
+            num=res;
         }
         return num;
     }
